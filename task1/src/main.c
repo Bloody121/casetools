a9d18e61c6ba875701da8f142ad0f05d46deece5
Алексей <lexa.2tri@yandex.ru>
2020-12-02 09:23:59 +0300

    initial commit

---
+++ task1/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "func1.h"
+
+int main(void)
+{
+    int i = 5;
+    printf("LOL KEK CHEBUREK\n");
+    print_hello();
+    printf("Факториал %d = %d.\n", i, factorial(i));
+    return 0;
+}
